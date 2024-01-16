4cbc44fdbe9b82e56d1c98630eb2344b38299584
Elham Zakizadeh <e.zakizadeh@outlook.com>
2024-01-16 15:24:04 +0330

    initialzed repository

---
+++ DevCard_project/DevCard_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevCard_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
