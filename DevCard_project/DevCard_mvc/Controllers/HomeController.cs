a2a74094188ff10d3d67ae0b4711bdd11c52a893
Elham Zakizadeh <e.zakizadeh@outlook.com>
2024-01-17 11:13:11 +0330

    add template

--- DevCard_project/DevCard_mvc/Controllers/HomeController.cs
+++ DevCard_project/DevCard_mvc/Controllers/HomeController.cs
@@ -6,19 +6,14 @@ namespace DevCard_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
