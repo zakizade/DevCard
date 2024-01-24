e011332d062fc43b570136f77df6400e1d6cee35
zakizade <e.zakizadeh1996@gmail.com>
2024-01-24 11:57:05 +0330

    project&LatestArticles move to view componnent

--- DevCard_project/DevCard_mvc/Components/ProjectsViewComponent.cs
+++ DevCard_project/DevCard_mvc/Components/ProjectsViewComponent.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using DevCard_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DevCard_mvc.Components
 {
-    public class ProjectsViewComponent: ViewComponent
+    public class ProjectsViewComponent : ViewComponent
     {
-        public IViewComponentResult invoke()
-        { 
-        return View("_Projects");
+        public IViewComponentResult Invoke(string name)
+        {
+            var projects= new List<Project>
+            {
+                new Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفر های درون شهری", "project-1.jpg", "Atriya"),
+                new Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور", "project-2.jpg","ZoodFood"),
+                new Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "project-3.jpg","MONOP"),
+                new Project(4, "فضاپیما", "برنامه مدیریت فضاپیما های ناسا","project-4.jpg", "NASA"),
+            };
+            return View("_Projects", projects);
         }
-        
+
     }
 }
--- DevCard_project/DevCard_mvc/Models/Project.cs
+++ DevCard_project/DevCard_mvc/Models/Project.cs
@@ -7,5 +7,15 @@
         public string Description { get; set; }
         public string Image { get; set; }
         public string Client { get; set; }
+
+
+        public Project(long id, string name, string description, string image, string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
     }
 }
